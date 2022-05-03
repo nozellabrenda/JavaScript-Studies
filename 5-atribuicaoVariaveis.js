87009e6cdd422ca348a339fb2900169d12cadcce
Brenda <brenda.nozella@gmail.com>
2022-05-03 20:22:20 -0300

    fix + 6

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,10 +1,19 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 27;
-let nome = "Brenda";
+
+const nome = "Brenda";
 const sobrenome = "Nozella";
 
+// 
 console.log(nome, sobrenome);
 console.log(`Meu nome é ${nome} ${sobrenome}`)
 
+let contador = 0;
+contador = contador + 1;
+
 const nomeCompelto = nome + sobrenome;
-console.log(nomeCompelto)
+console.log(nomeCompelto);
+
+let idade; //declarando variável
+idade = 27; // atribuindo valor
+idade - idade + 1;
+console.log(idade);
